c892f1facd1dc8fc535f9dd78af0a95996740243
olivierayache <olivier.ayache@gmail.com>
2018-02-15 03:48:23 +0100

    Improve Number support for return type of AsyncTask.

--- src/main/java/org/ayache/gwt/async/task/api/AsyncTask.java
+++ src/main/java/org/ayache/gwt/async/task/api/AsyncTask.java
@@ -29,12 +29,84 @@ import jsinterop.annotations.JsType;
 @SuppressWarnings("UseSpecificCatch")
 public abstract class AsyncTask<Params, Result> {
 
+    @JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
+    static class ParamsHolder {
+
+        private String type;
+        private Object params;
+        private String dataType;
+
+        @JsOverlay
+        static ParamsHolder build(String type, Object params) {
+            ParamsHolder paramsHolder = new ParamsHolder();
+            paramsHolder.type = type;
+            if (params instanceof String) {
+                paramsHolder.params = params;
+            } else if (params instanceof Number) {
+                paramsHolder.dataType = "Number";
+                paramsHolder.params = JavaScriptObject.createObject();
+                JSONObject jSONObject = new JSONObject((JavaScriptObject) paramsHolder.params);
+                jSONObject.put(paramsHolder.dataType, new JSONNumber(((Number) params).doubleValue()));
+            } else if (params instanceof JavaScriptObject) {
+                paramsHolder.params = (JavaScriptObject) params;
+            } else {
+                Logger.getLogger("").log(Level.SEVERE, "Params type of AsyncTask<Params, Result> must be JavascriptObject or @JsType annotated class");
+            }
+            return paramsHolder;
+        }
+
+    }
+
+    @JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
+    static class ResultHolder<Result> {
+
+        private Result result;
+        private String dataType;
+        private String exceptionType;
+        private String exceptionMessage;
+
+        @JsOverlay
+        static <Result> ResultHolder<Result> build(Result result) {
+            if (result instanceof String || result instanceof Double) {
+                ResultHolder<Result> resultHolder = new ResultHolder<>();
+                resultHolder.result = result;
+                return resultHolder;
+            } else if (result instanceof Number) {
+                ResultHolder<Number> resultHolder = new ResultHolder<>();
+                resultHolder.dataType = result.getClass().getName();
+                resultHolder.result = Double.valueOf(String.valueOf(result));
+                return (ResultHolder<Result>) resultHolder;
+            } else if (result instanceof JavaScriptObject) {
+                ResultHolder<JavaScriptObject> resultHolder = new ResultHolder<>();
+                resultHolder.result = (JavaScriptObject) result;
+                return (ResultHolder<Result>) resultHolder;
+            } else {
+                Logger.getLogger("").log(Level.SEVERE, "Result type of AsyncTask<Params, Result> must be JavascriptObject or @JsType annotated class");
+            }
+            return new ResultHolder<>();
+        }
+
+        @JsOverlay
+        static ResultHolder build(Throwable result) {
+            ResultHolder resultHolder = new ResultHolder();
+            resultHolder.exceptionType = result.getClass().getName();
+            resultHolder.exceptionMessage = result.getMessage();
+            return resultHolder;
+        }
+
+        @JsOverlay
+        private boolean failed() {
+            return exceptionType != null;
+        }
+
+    }
+
     static {
         setOnMessage((MessageListener<ParamsHolder>) (MessageEvent<ParamsHolder> s) -> {
             if (!s.getData().type.matches("[a-z]+\\.([a-z]+\\.)*([a-zA-Z][A-Za-z\\d]*\\$?)+")) {
                 throw new IllegalArgumentException(s.getData().type + " is not a valid class name. Implementation of AsyncTask can't be anonymous inner class.");
             }
-            AsyncTask construct = evalQuietly("new "+s.getData().type.replace('$', '.'));
+            AsyncTask construct = evalQuietly("new " + s.getData().type.replace('$', '.'));
             if (construct == null) {
                 throw new IllegalArgumentException(s.getData().type + " must be annotated with @JsType");
             }
@@ -58,119 +130,6 @@ public abstract class AsyncTask<Params, Result> {
             }
         });
     }
-
-    @JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
-    static class ParamsHolder {
-
-        private String type;
-        private Object params;
-        private String dataType;
-
-        @JsOverlay
-        static ParamsHolder build(String type, Object params) {
-            ParamsHolder paramsHolder = new ParamsHolder();
-            paramsHolder.type = type;
-            if (params instanceof String) {
-                paramsHolder.params = params;
-            } else if (params instanceof Number) {
-                paramsHolder.dataType = "Number";
-                paramsHolder.params = JavaScriptObject.createObject();
-                JSONObject jSONObject = new JSONObject((JavaScriptObject) paramsHolder.params);
-                jSONObject.put(paramsHolder.dataType, new JSONNumber(((Number) params).doubleValue()));
-            } else {
-                paramsHolder.params = (JavaScriptObject) params;
-            }
-            return paramsHolder;
-        }
-
-    }
-
-    @JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
-    static class ResultHolder<Result> {
-
-        private Result result;
-        private String exceptionType;
-        private String exceptionMessage;
-
-        @JsOverlay
-        static <Result> ResultHolder<Result> build(Result result) {
-            if (result instanceof String || result instanceof Number) {
-                ResultHolder<Result> resultHolder = new ResultHolder<>();
-                resultHolder.result = result;
-                return resultHolder;
-            } else {
-                ResultHolder<JavaScriptObject> resultHolder = new ResultHolder<>();
-                resultHolder.result = (JavaScriptObject) result;
-                return (ResultHolder<Result>) resultHolder;
-            }
-
-        }
-
-        @JsOverlay
-        static ResultHolder build(Throwable result) {
-            ResultHolder resultHolder = new ResultHolder();
-            resultHolder.exceptionType = result.getClass().getName();
-            resultHolder.exceptionMessage = result.getMessage();
-            return resultHolder;
-        }
-
-        @JsOverlay
-        private boolean failed() {
-            return exceptionType != null;
-        }
-
-    }
-
-    private Worker<ParamsHolder> worker;
-
-    public void execute(Params p) {
-        if (worker == null) {
-            worker = new Worker(GWT.<WorkerScriptFactory>create(WorkerScriptFactory.class).getWorkerScriptURL());
-            worker.setOnMessage((MessageListener<ResultHolder<Result>>) (MessageEvent<ResultHolder<Result>> s) -> {
-                if (s.getData().failed()) {
-                    onError(s.getData().exceptionType, s.getData().exceptionMessage);
-                } else {
-                    done(s.getData().result);
-                }
-            });
-        }
-        ParamsHolder paramsHolder = ParamsHolder.build(getClass().getName(), p);
-        Object[] transferList = getTransferList(paramsHolder.params);
-        try {
-            if (transferList != null) {
-                worker.postMessage(paramsHolder, transferList);
-            } else {
-                worker.postMessage(paramsHolder);
-            }
-        } catch (Exception ex) {
-            Logger.getLogger("").log(Level.SEVERE, "Params type of AsyncTask<Params, Result> must be JavascriptObject or @JsType annotated class :", ex);
-        }
-    }
-
-    protected abstract Result doInBackground(Params p) throws Exception;
-
-    protected abstract void done(Result r);
-
-    protected void onError(String type, String message) {
-    }
-
-    @JsProperty(namespace = JsPackage.GLOBAL, name = "onmessage")
-    private static native void setOnMessage(MessageListener listener);
-
-    @JsMethod(namespace = JsPackage.GLOBAL, name = "postMessage")
-    private static native <Transferable> void postMessage(ResultHolder p);
-
-    @JsMethod(namespace = JsPackage.GLOBAL, name = "postMessage")
-    private static native <Transferable> void postMessage(ResultHolder p, Transferable[] transferList);
-
-    @JsMethod(namespace = JsPackage.GLOBAL, name = "eval")
-    private static native <T> T eval(String s) throws Exception;
-
-    @JsMethod(namespace = "console", name = "error")
-    protected static native void error(Object o);
-
-    @JsMethod(namespace = JsPackage.GLOBAL, name = "Object.create")
-    protected static native <T,P> T create(P prototype);
 
     private static <T> T evalQuietly(String s) {
         try {
@@ -200,5 +159,69 @@ public abstract class AsyncTask<Params, Result> {
             return transferList.toArray();
         }
     }
+
+    private Worker<ParamsHolder> worker;
+
+    public void execute(Params p) {
+        if (worker == null) {
+            worker = new Worker(GWT.<WorkerScriptFactory>create(WorkerScriptFactory.class
+            ).getWorkerScriptURL());
+            worker.setOnMessage((MessageListener<ResultHolder<Result>>) (MessageEvent<ResultHolder<Result>> s) -> {
+                if (s.getData().failed()) {
+                    onError(s.getData().exceptionType, s.getData().exceptionMessage);
+                } else {
+                    if (s.getData().dataType != null) {
+                        done(castDoubleTo(s.getData().dataType, (Double) s.getData().result));
+                    } else {
+                        done(s.getData().result);
+                    }
+                }
+            });
+        }
+        ParamsHolder paramsHolder = ParamsHolder.build(getClass().getName(), p);
+        Object[] transferList = getTransferList(paramsHolder.params);
+        if (transferList != null) {
+            worker.postMessage(paramsHolder, transferList);
+        } else {
+            worker.postMessage(paramsHolder);
+        }
+    }
+
+    protected abstract Result doInBackground(Params p) throws Exception;
+
+    protected abstract void done(Result r);
+
+    protected void onError(String type, String message) {
+    }
+
+    private <T> T castDoubleTo(String type, Double o) {
+        if (type.equals(Integer.class.getName())) {
+            return (T) Integer.valueOf(o.toString());
+        } else if (type.equals(Long.class.getName())) {
+            return (T) Long.valueOf(o.toString());
+        } else if (type.equals(Short.class.getName())) {
+            return (T) Short.valueOf(o.toString());
+        } else if (type.equals(Byte.class.getName())) {
+            return (T) Byte.valueOf(o.toString());
+        } else if (type.equals(Float.class.getName())) {
+            return (T) Float.valueOf(o.toString());
+        }
+        return (T) o;
+    }
+
+    @JsProperty(namespace = JsPackage.GLOBAL, name = "onmessage")
+    private static native void setOnMessage(MessageListener listener);
+
+    @JsMethod(namespace = JsPackage.GLOBAL, name = "postMessage")
+    private static native <Transferable> void postMessage(ResultHolder p);
+
+    @JsMethod(namespace = JsPackage.GLOBAL, name = "postMessage")
+    private static native <Transferable> void postMessage(ResultHolder p, Transferable[] transferList);
+
+    @JsMethod(namespace = JsPackage.GLOBAL, name = "eval")
+    private static native <T> T eval(String s) throws Exception;
+
+    @JsMethod(namespace = "console", name = "error")
+    protected static native void error(Object o);
 
 }
--- src/main/java/org/ayache/gwt/async/task/api/WorkerScriptDevModeFactory.java
+++ src/main/java/org/ayache/gwt/async/task/api/WorkerScriptDevModeFactory.java
@@ -15,15 +15,21 @@ import com.google.gwt.typedarrays.shared.ArrayBuffer;
  */
 class WorkerScriptDevModeFactory extends WorkerScriptFactory {
 
-    @Override
-    public String getWorkerScriptURL() {
+    private static final String URL;
+
+    static {
         XMLHttpRequest create = XMLHttpRequest.create();
         create.open("GET", GWT.getModuleBaseURL().replace("8888", "9876") + GWT.getModuleName() + ".worker.js", false);
         create.send();
         Blob.Options options = new Blob.Options();
         options.type = "application/javascript";
         Blob blob = new Blob(new ArrayBuffer[]{JsUtils.arrayBufferFromString(create.getResponseText())}, options);
-        return Blob.createObjectURL(blob);
+        URL = Blob.createObjectURL(blob);
+    }
+
+    @Override
+    public String getWorkerScriptURL() {
+        return URL;
     }
 
 }
